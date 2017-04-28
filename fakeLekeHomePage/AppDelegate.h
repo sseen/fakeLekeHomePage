785d4b315747b07f253edc62866a971bf544a386
sseen <ssnfee@gmail.com>
2017-04-28 15:59:16 +0800

    Initial Commit

---
+++ fakeLekeHomePage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fakeLekeHomePage
+//
+//  Created by sseen on 2017/4/28.
+//  Copyright © 2017年 sseen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
