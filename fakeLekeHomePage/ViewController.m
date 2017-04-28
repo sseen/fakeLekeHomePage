6a30c2d35cb7219301ebe31bd6d1ef69ba914e2a
sseen <ssnfee@gmail.com>
2017-04-28 17:17:32 +0800

    add

--- fakeLekeHomePage/ViewController.m
+++ fakeLekeHomePage/ViewController.m
@@ -8,8 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource,  UIScrollViewDelegate>
 
+@property (weak, nonatomic) IBOutlet UITableView *mainTable;
+@property (assign, nonatomic) CGFloat lastOffsetY ;
 @end
 
 @implementation ViewController
@@ -17,6 +19,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.frame), 100)];
+    view.backgroundColor = [UIColor lightGrayColor];
+    
+    [_mainTable registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell0"];
+    _mainTable.decelerationRate = UIScrollViewDecelerationRateFast;
+    _mainTable.tableHeaderView = view;
 }
 
 
@@ -25,5 +33,57 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+#pragma mark - table
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 40;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell0"];
+    cell.textLabel.text = [ NSString stringWithFormat:@"item %ld",(long)indexPath.row ];
+    
+    return cell;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    UIView *contentView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.frame), 40)];
+    UILabel *lblName = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 100, 40)];
+    lblName.textColor = [UIColor purpleColor];
+    lblName.text = @"title";
+    [contentView addSubview:lblName];
+    
+    return contentView;
+}
+
+#pragma mark - scroll
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    
+    CGFloat offset = scrollView.contentOffset.y ;
+    
+    NSLog(@"%f", offset);
+    
+}
+
+- (void)scrollViewWillBeginDragging:(UIScrollView*)scrollView {
+    self.lastOffsetY = scrollView.contentOffset.y;
+}
+
+- (void)scrollViewWillEndDragging:(UIScrollView *)scrollView withVelocity:(CGPoint)velocity targetContentOffset:(inout CGPoint *)targetContentOffset {
+    if (scrollView.contentOffset.y > self.lastOffsetY) {
+        scrollView.contentOffset = CGPointMake(0, 100);
+        
+    }
+}
 
 @end
