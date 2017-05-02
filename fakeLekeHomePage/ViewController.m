ef02d5d3be989f4eedb5a1c2d661207f197aa3e0
sseen <ssnfee@gmail.com>
2017-05-02 17:28:03 +0800

    Partial showing header

--- fakeLekeHomePage/ViewController.m
+++ fakeLekeHomePage/ViewController.m
@@ -7,13 +7,19 @@
 //
 
 #import "ViewController.h"
+typedef NS_ENUM(NSInteger, HomeHeaderState) {
+    HomeHeaderStateHided,
+    HomeHeaderStateShowing,
+    HomeHeaderStatePartial
+};
 
-@interface ViewController () <UITableViewDelegate, UITableViewDataSource,  UIScrollViewDelegate>
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate, UICollectionViewDelegate, UICollectionViewDataSource>
 
 @property (weak, nonatomic) IBOutlet UITableView *mainTable;
 @property (strong, nonatomic) UIView *headerView;
+@property (strong, nonatomic) UICollectionView *mainCollection;
 
-@property (assign, nonatomic) Boolean isUp;
+@property (assign, nonatomic) HomeHeaderState isUp; // -1:hide 0:part of show 1:all show
 @property (assign, nonatomic) Boolean velocity;
 @end
 
@@ -22,6 +28,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    _isUp = 1;
+    
     self.headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 64, CGRectGetWidth(self.view.frame), 100)];
     _headerView.backgroundColor = [UIColor lightGrayColor];
     
@@ -30,6 +39,17 @@
     [self.view addSubview: _headerView];
     
     _mainTable.scrollIndicatorInsets = UIEdgeInsetsMake(40, 0, 0, 0);
+//
+//    self.mainCollection = [[UICollectionView alloc] init];
+//    UICollectionViewFlowLayout *layout=[[UICollectionViewFlowLayout alloc] init];
+//    _mainCollection=[[UICollectionView alloc] initWithFrame:self.view.frame collectionViewLayout:layout];
+//    [_mainCollection setDataSource:self];
+//    [_mainCollection setDelegate:self];
+//    
+//    [_mainCollection registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cellIdentifier"];
+//    [_mainCollection setBackgroundColor:[UIColor redColor]];
+//    
+//    [self.view addSubview:_mainCollection];
 }
 
 
@@ -37,6 +57,9 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - collection
+
 
 
 #pragma mark - table
@@ -111,8 +134,6 @@
             }
             case UIGestureRecognizerStateEnded: {
                 
-                
-                
                 break;
             }
             
@@ -127,23 +148,36 @@
     
     // isUp : header show or hide
     // velocity : because 主动调用 scrollViewDidEndDecelerating 后，scrollview 还会在滑动结束后自己调用一次，加上加速度代表是主动调用
-    NSLog(@"%d, %d, %f", _isUp, _velocity, scrollView.contentOffset.y);
-    if (_isUp && !_velocity && scrollView.contentOffset.y<10) {// header disappear will show
-        [UIView animateWithDuration:0.2 delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
-            _headerView.frame = CGRectMake(0, 64, CGRectGetWidth(_headerView.frame), CGRectGetHeight(_headerView.frame));
-            _mainTable.frame = CGRectMake(0, 164, CGRectGetWidth(scrollView.frame), CGRectGetHeight(scrollView.frame));
-        } completion:nil];
-        
-        _isUp = !_isUp;
+    // velocity negative discripe up, active down
+    NSLog(@"%ld, %d, %f", (long)_isUp, _velocity, scrollView.contentOffset.y);
+    if (_isUp!=HomeHeaderStateShowing && !_velocity) {// scroll down, header disappear will show
+        if ( scrollView.contentOffset.y<10) {
+            
+            [UIView animateWithDuration:0.2 delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
+                _headerView.frame = CGRectMake(0, 64, CGRectGetWidth(_headerView.frame), CGRectGetHeight(_headerView.frame));
+                _mainTable.frame = CGRectMake(0, 164, CGRectGetWidth(scrollView.frame), CGRectGetHeight(scrollView.frame));
+            } completion:nil];
+            
+            _isUp = HomeHeaderStateShowing;
+        } else {
+            [UIView animateWithDuration:0.2 delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
+                _headerView.frame = CGRectMake(0, 64 - 50, CGRectGetWidth(_headerView.frame), CGRectGetHeight(_headerView.frame));
+                _mainTable.frame = CGRectMake(0, 164 - 50, CGRectGetWidth(scrollView.frame), CGRectGetHeight(scrollView.frame));
+            } completion:nil];
+            
+            _isUp = HomeHeaderStatePartial;
+        }
 
     }
-    if (!_isUp && _velocity) {// header show will hide
+    if (_velocity) {// header show will hide
         [UIView animateWithDuration:0.2 delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
             _headerView.frame = CGRectMake(0, -36, CGRectGetWidth(_headerView.frame), CGRectGetHeight(_headerView.frame));
             _mainTable.frame = CGRectMake(0, 64, CGRectGetWidth(scrollView.frame), CGRectGetHeight(scrollView.frame));
         } completion:nil];
         
-         _isUp = !_isUp;
+        if (_isUp==HomeHeaderStateShowing) {
+            _isUp = HomeHeaderStateHided;
+        }
     }
     
 }
