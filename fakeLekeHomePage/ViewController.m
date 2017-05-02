9f733b35985387ca321cc614e23b151d51e6bcc2
sseen <ssnfee@gmail.com>
2017-05-02 14:59:16 +0800

    done

--- fakeLekeHomePage/ViewController.m
+++ fakeLekeHomePage/ViewController.m
@@ -29,6 +29,8 @@
     [_mainTable registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell0"];
     _mainTable.decelerationRate = UIScrollViewDecelerationRateFast;
     [self.view addSubview: _headerView];
+    
+    _mainTable.scrollIndicatorInsets = UIEdgeInsetsMake(40, 0, 0, 0);
 }
 
 
@@ -49,7 +51,7 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 20;
+    return 80;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -64,6 +66,7 @@
     UILabel *lblName = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 100, 40)];
     lblName.textColor = [UIColor purpleColor];
     lblName.text = @"title";
+    contentView.backgroundColor = [UIColor cyanColor];
     [contentView addSubview:lblName];
     
     return contentView;
@@ -75,17 +78,15 @@
     
     CGFloat offset = scrollView.contentOffset.y ;
     
-    NSLog(@"%f", offset);
+    NSLog(@"%f, %ld", offset, (long)scrollView.panGestureRecognizer.state);
     
-    if (scrollView.contentOffset.y<_lastOffsetY) {
-        NSLog(@"down");
-    } else if (scrollView.contentOffset.y>_lastOffsetY) {
-        NSLog(@"up");
-    }
+//    if (scrollView.contentOffset.y<_lastOffsetY) {
+//        NSLog(@"down");
+//    } else if (scrollView.contentOffset.y>_lastOffsetY) {
+//        NSLog(@"up");
+//    }
     
-//    CGFloat velocityY = [scrollView.panGestureRecognizer  velocityInView:self.view].y;
-//    NSLog(@"ss %f",velocityY);
-//    self.velocity = velocityY > 0 ? true : false;
+    
     
     if ([scrollView isEqual:_mainTable]) {
         
@@ -96,15 +97,24 @@
                 NSLog(@"ss %f",velocityY);
                 self.velocity = velocityY < 0 ? true : false;
                 [[scrollView delegate] scrollViewDidEndDecelerating:scrollView];
+                
                 // User began dragging
                 break;
             }
-            case UIGestureRecognizerStateChanged:
+            case UIGestureRecognizerStateChanged: {
+                CGFloat velocityY = [scrollView.panGestureRecognizer  velocityInView:self.view].y;
+                NSLog(@"ss %f",velocityY);
+                self.velocity = velocityY < 0 ? true : false;
+                [[scrollView delegate] scrollViewDidEndDecelerating:scrollView];
                 
                 // User is currently dragging the scroll view
                 break;
-                
+            }
             case UIGestureRecognizerStatePossible: {
+                
+                if (scrollView.contentOffset.y<10) {
+                    [[scrollView delegate] scrollViewDidEndDecelerating:scrollView];
+                }
                 
                 // The scroll view scrolling but the user is no longer touching the scrollview (table is decelerating)
                 break;
@@ -130,22 +140,25 @@
 
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
     
-    if (_isUp && !_velocity) {// header disappear will show
-        [UIView animateWithDuration:0.4 delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
+    // isUp : header show or hide
+    // velocity : because 主动调用 scrollViewDidEndDecelerating 后，scrollview 还会在滑动结束后自己调用一次，加上加速度代表是主动调用
+    NSLog(@"%d, %d, %f", _isUp, _velocity, scrollView.contentOffset.y);
+    if (_isUp && !_velocity && scrollView.contentOffset.y<10) {// header disappear will show
+        [UIView animateWithDuration:0.2 delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
             _headerView.frame = CGRectMake(0, 64, CGRectGetWidth(_headerView.frame), CGRectGetHeight(_headerView.frame));
-            scrollView.frame = CGRectMake(0, 164, CGRectGetWidth(scrollView.frame), CGRectGetHeight(scrollView.frame));
+            _mainTable.frame = CGRectMake(0, 164, CGRectGetWidth(scrollView.frame), CGRectGetHeight(scrollView.frame));
         } completion:nil];
         
         _isUp = !_isUp;
 
     }
     if (!_isUp && _velocity) {// header show will hide
-        [UIView animateWithDuration:0.4 delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
+        [UIView animateWithDuration:0.2 delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
             _headerView.frame = CGRectMake(0, -36, CGRectGetWidth(_headerView.frame), CGRectGetHeight(_headerView.frame));
-            scrollView.frame = CGRectMake(0, 0, CGRectGetWidth(scrollView.frame), CGRectGetHeight(scrollView.frame));
+            _mainTable.frame = CGRectMake(0, 64, CGRectGetWidth(scrollView.frame), CGRectGetHeight(scrollView.frame));
         } completion:nil];
         
-        _isUp = !_isUp;
+         _isUp = !_isUp;
     }
     
 }
