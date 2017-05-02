9035fff1a8286230fd246baf8503b88868a1a55c
sseen <ssnfee@gmail.com>
2017-05-02 18:02:30 +0800

    update

--- fakeLekeHomePage/ViewController.m
+++ fakeLekeHomePage/ViewController.m
@@ -13,14 +13,18 @@ typedef NS_ENUM(NSInteger, HomeHeaderState) {
     HomeHeaderStatePartial
 };
 
-@interface ViewController () <UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate, UICollectionViewDelegate, UICollectionViewDataSource>
+float marginItem = 20;
+
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
 
 @property (weak, nonatomic) IBOutlet UITableView *mainTable;
 @property (strong, nonatomic) UIView *headerView;
+@property (strong, nonatomic) UIView *headerViewDel;
 @property (strong, nonatomic) UICollectionView *mainCollection;
 
 @property (assign, nonatomic) HomeHeaderState isUp; // -1:hide 0:part of show 1:all show
 @property (assign, nonatomic) Boolean velocity;
+@property (assign, nonatomic) CGFloat screenWidth;
 @end
 
 @implementation ViewController
@@ -30,26 +34,27 @@ typedef NS_ENUM(NSInteger, HomeHeaderState) {
     // Do any additional setup after loading the view, typically from a nib.
     
     _isUp = 1;
+    _screenWidth = CGRectGetWidth(self.view.frame);
     
-    self.headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 64, CGRectGetWidth(self.view.frame), 100)];
+    self.headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, _screenWidth, 100)];
     _headerView.backgroundColor = [UIColor lightGrayColor];
     
     [_mainTable registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell0"];
     _mainTable.decelerationRate = UIScrollViewDecelerationRateFast;
-    [self.view addSubview: _headerView];
-    
     _mainTable.scrollIndicatorInsets = UIEdgeInsetsMake(40, 0, 0, 0);
-//
-//    self.mainCollection = [[UICollectionView alloc] init];
-//    UICollectionViewFlowLayout *layout=[[UICollectionViewFlowLayout alloc] init];
-//    _mainCollection=[[UICollectionView alloc] initWithFrame:self.view.frame collectionViewLayout:layout];
-//    [_mainCollection setDataSource:self];
-//    [_mainCollection setDelegate:self];
-//    
-//    [_mainCollection registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cellIdentifier"];
-//    [_mainCollection setBackgroundColor:[UIColor redColor]];
-//    
-//    [self.view addSubview:_mainCollection];
+
+    UICollectionViewFlowLayout *layout=[[UICollectionViewFlowLayout alloc] init];
+    layout.headerReferenceSize = CGSizeMake(_screenWidth, 100.f);
+    self.mainCollection = [[UICollectionView alloc] initWithFrame:CGRectMake(0, 64, _screenWidth, 100 + 200) collectionViewLayout:layout];
+
+    [_mainCollection setDataSource:self];
+    [_mainCollection setDelegate:self];
+    
+    [_mainCollection registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cellIdentifier"];
+    
+    [self.view addSubview:_mainCollection];
+    
+    self.headerViewDel = _mainCollection;
 }
 
 
@@ -60,7 +65,55 @@ typedef NS_ENUM(NSInteger, HomeHeaderState) {
 
 #pragma mark - collection
 
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 5;
+}
 
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell=[collectionView dequeueReusableCellWithReuseIdentifier:@"cellIdentifier" forIndexPath:indexPath];
+    cell.backgroundColor=[UIColor darkGrayColor];
+    return cell;
+}
+
+
+#pragma mark - flow
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    float width = [self itemWidth];
+    return CGSizeMake(width, width);
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section {
+    return [self itemSpacing];
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section {
+    return [self itemSpacing] / 2;
+}
+
+// Layout: Set Edges
+- (UIEdgeInsets)collectionView:
+(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
+    return UIEdgeInsetsMake(0,20,0,20);  // top, left, bottom, right
+    // return UIEdgeInsetsMake(0,8,0,8);
+}
+
+- (float)itemSpacing {
+    int cellsInLine = 4;
+    float width = [self itemWidth];
+    // item space
+    float spacing = ((CGRectGetWidth(self.view.frame) - cellsInLine * width - marginItem * 2) / (cellsInLine -1));
+    
+    return spacing;
+}
+
+- (float)itemWidth {
+    int cellsInLine = 6;
+    float width = roundf(( CGRectGetWidth(self.view.frame) - cellsInLine + 1) / cellsInLine);
+    return width;
+}
 
 #pragma mark - table
 
@@ -150,28 +203,28 @@ typedef NS_ENUM(NSInteger, HomeHeaderState) {
     // velocity : because 主动调用 scrollViewDidEndDecelerating 后，scrollview 还会在滑动结束后自己调用一次，加上加速度代表是主动调用
     // velocity negative discripe up, active down
     NSLog(@"%ld, %d, %f", (long)_isUp, _velocity, scrollView.contentOffset.y);
-    if (_isUp!=HomeHeaderStateShowing && !_velocity) {// scroll down, header disappear will show
+    if (_isUp!=HomeHeaderStateShowing && !_velocity) {// scroll up, header disappear will show
         if ( scrollView.contentOffset.y<10) {
             
             [UIView animateWithDuration:0.2 delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
-                _headerView.frame = CGRectMake(0, 64, CGRectGetWidth(_headerView.frame), CGRectGetHeight(_headerView.frame));
-                _mainTable.frame = CGRectMake(0, 164, CGRectGetWidth(scrollView.frame), CGRectGetHeight(scrollView.frame));
+                _headerViewDel.frame = CGRectMake(0, 64, CGRectGetWidth(_headerViewDel.frame), CGRectGetHeight(_headerViewDel.frame));
+                _mainTable.frame = CGRectMake(0, CGRectGetHeight(_headerViewDel.frame) + 64, CGRectGetWidth(scrollView.frame), CGRectGetHeight(scrollView.frame));
             } completion:nil];
             
             _isUp = HomeHeaderStateShowing;
         } else {
             [UIView animateWithDuration:0.2 delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
-                _headerView.frame = CGRectMake(0, 64 - 50, CGRectGetWidth(_headerView.frame), CGRectGetHeight(_headerView.frame));
-                _mainTable.frame = CGRectMake(0, 164 - 50, CGRectGetWidth(scrollView.frame), CGRectGetHeight(scrollView.frame));
+                _headerViewDel.frame = CGRectMake(0, 64 - 100, CGRectGetWidth(_headerViewDel.frame), CGRectGetHeight(_headerViewDel.frame));
+                _mainTable.frame = CGRectMake(0, 164 - 100, CGRectGetWidth(scrollView.frame), CGRectGetHeight(scrollView.frame));
             } completion:nil];
             
             _isUp = HomeHeaderStatePartial;
         }
 
     }
-    if (_velocity) {// header show will hide
+    if (_velocity) {//scroll down, header show will hide
         [UIView animateWithDuration:0.2 delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
-            _headerView.frame = CGRectMake(0, -36, CGRectGetWidth(_headerView.frame), CGRectGetHeight(_headerView.frame));
+            _headerViewDel.frame = CGRectMake(0, 64 - CGRectGetHeight(_headerViewDel.frame) , CGRectGetWidth(_headerViewDel.frame), CGRectGetHeight(_headerViewDel.frame));
             _mainTable.frame = CGRectMake(0, 64, CGRectGetWidth(scrollView.frame), CGRectGetHeight(scrollView.frame));
         } completion:nil];
         
